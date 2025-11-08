b08ab2870724d610ca82a450da89feb29de426cb
Mostakin <mostakin4556@gmail.com>
2025-11-08 15:21:22 +0600

    update Stock API

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,10 @@
+import FuzzyText from "@/components/FuzzyText";
 import DashboardCard from "../components/DashboardCard";
 import Navbar from "../components/Navbar";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="">
       <Navbar pageTitle="Dashboard" />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-2">
@@ -11,6 +12,25 @@ export default function Dashboard() {
         <DashboardCard title="Total Orders" value={75} color="bg-green-500" />
         <DashboardCard title="Total Users" value={50} color="bg-yellow-500" />
         <DashboardCard title="Stock Alerts" value={5} color="bg-red-500" />
+      </div>
+
+      <div className="flex flex-col justify-center items-center w-full h-full md:mt-40 mt-5">
+        <div className="">
+          <img className="w-[200px]" src="logo.png" alt="Victus Byte Logo" />
+        </div>
+
+        {/* Uncomment this if you want fuzzy text next to the logo */}
+
+        <div className="mt-10 -mr-10 hidden md:flex">
+          <FuzzyText
+            baseIntensity={0.2}
+            hoverIntensity={0.5}
+            enableHover={true}
+            color="#1F2937" // Tailwind gray-800
+          >
+            VICTUS BYTE
+          </FuzzyText>
+        </div>
       </div>
     </div>
   );
